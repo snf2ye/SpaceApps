a28980f7726ff909b30da9164377d767ed960155
Bobby Holden <bgh9qs@virginia.edu>
2017-04-29 16:54:56 -0500

    Added jquery include, csv parsing and iterative marker placement

--- js/place_state_parks.js
+++ js/place_state_parks.js
@@ -1,7 +1,31 @@
-place_marker(-86.5861, 34.7304, 'http://localhost:8080/resources/images/arrowhead.jpg');
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "/resources/data/NationalParks.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+});
 
+var lines = [];
+function processData(allText) {
+    var record_num = 3;  // or however many elements there are in each row
+    var allTextLines = allText.split(/\r\n|\n/);
+    var entries = allTextLines[0].split(',');
 
-function place_marker(long, lat, marker) {
+    var headings = entries.splice(0,record_num);
+    while (entries.length>1) { //for some reason is spitting up undefined row on the last, so exclude it
+        var tarr = [];
+        for (var j=0; j<record_num; j++) {
+            var val = entries.shift();
+            tarr.push(val);
+        }
+		place_marker(tarr[2], tarr[1], tarr[0].replace(/\"/g, ""), '/resources/images/arrowhead.jpg');        
+        lines.push(tarr);
+    }
+}
+
+function place_marker(long, lat, name, marker) {
   viewer.entities.add({
     position : Cesium.Cartesian3.fromDegrees(long, lat),
     billboard : {
@@ -10,7 +34,7 @@ function place_marker(long, lat, marker) {
         height : 16
     },
     label : {
-    text : 'National Park',
+    text : name,
       font : '14pt monospace',
       style: Cesium.LabelStyle.FILL_AND_OUTLINE,
       outlineWidth : 2,
