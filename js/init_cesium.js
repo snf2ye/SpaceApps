9778f9e7d53fc44b9d6918d9898d35fb8c68676a
Bobby Holden <bgh9qs@virginia.edu>
2017-04-30 00:17:16 -0500

    Added proper Bing Key

--- js/init_cesium.js
+++ js/init_cesium.js
@@ -1,3 +1,4 @@
+Cesium.BingMapsApi.defaultKey = 'Aou0pmPMbGyDeG1aXoHc4veeGTPqnh_32hxgp3Rx2ybLiiMdnHvFKHROIR7AaMpg';
 var viewer = new Cesium.Viewer('cesiumContainer');
 var terrainProvider = new Cesium.CesiumTerrainProvider({
     url : '//assets.agi.com/stk-terrain/world'
